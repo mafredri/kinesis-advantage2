9aced4eb4e5d65a65a78ccbdff17e5517ad36676
Mathias Fredriksson <mafredri@gmail.com>
2018-08-27 16:04:46 +0300

    Check if volume exists in scripts

--- push.sh
+++ push.sh
@@ -4,5 +4,9 @@ DIR=${0:A:h}
 VOL=/Volumes/KINESIS\ KB
 
 (cd $DIR;
+	[[ -d $VOL ]] || {
+		print "error: volume is not mounted ($VOL)"
+		exit 1
+	}
 	rsync --exclude-from .gitignore -rav ${VOL:t} /Volumes/
 )
--- sync.sh
+++ sync.sh
@@ -4,6 +4,10 @@ DIR=${0:A:h}
 VOL=/Volumes/KINESIS\ KB
 
 (cd $DIR;
+	[[ -d $VOL ]] || {
+		print "error: volume is not mounted ($VOL)"
+		exit 1
+	}
 	rsync -rav $VOL ./
 	git add .
 	git commit -v -m 'Sync config'
